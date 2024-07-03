62fc8fe2bf04b3fdb396ae80ff20335b08619b81
Андрей Хохлов <andrey@MacBook-Air-Andrej.local>
2024-07-04 01:09:43 +0300

    Add new file App.module.css, modified file App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import { TimeComponent } from './components/time';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 export const App = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
+		<div className={styles.app}>
+			<div className={styles.display}>123+100</div>
+			<ul className={styles.list}>
+				<li className={styles.item + ' ' + styles['item--reset']}>
+					<button className={styles.button + ' ' + styles['button--reset']}>
+						C
+					</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>7</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>8</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>9</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>4</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>5</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>6</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>1</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>2</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.button}>3</button>
+				</li>
+				<li className={styles.item + ' ' + styles['item--zero']}>
+					<button className={styles.button}>0</button>
+				</li>
+				<li
+					className={
+						styles.item +
+						' ' +
+						styles['item--operator'] +
+						' ' +
+						styles['item--operator-plus']
+					}
 				>
-					Learn React
-				</a>
-				<TimeComponent />
-			</header>
+					<button className={styles.button + ' ' + styles['button--operator']}>
+						+
+					</button>
+				</li>
+				<li className={styles.item + ' ' + styles['item--operator']}>
+					<button className={styles.button + ' ' + styles['button--operator']}>
+						-
+					</button>
+				</li>
+				<li className={styles.item + ' ' + styles['item--enter']}>
+					<button className={styles.button + ' ' + styles['button--enter']}>
+						=
+					</button>
+				</li>
+			</ul>
 		</div>
 	);
 };
