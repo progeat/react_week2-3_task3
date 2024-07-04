ca55e0d919459478bda14897e1bffc41ff03e7a0
Хохлов Андрей Анатольевич <A.Hohlov@transtelematica.ru>
2024-07-04 16:38:58 +0300

    Add file data.js and modified file App.js, App.module.css, index.css

--- src/App.js
+++ src/App.js
@@ -1,68 +1,79 @@
+import { useState } from 'react';
+import data from './data.json';
 import styles from './App.module.css';
 
 export const App = () => {
+	const [operand1, setOperand1] = useState('');
+	const [operator, setOperator] = useState('');
+	const [operand2, setOperand2] = useState('');
+
+	const onClickButton = ({ target }) => {
+		const value = target.dataset.value;
+		if (Number(value)) {
+			if (operator === '') return setOperand1((operand1) => operand1 + value);
+			setOperand2((operand2) => operand2 + value);
+		} else if (value === '+' || value === '-') {
+			setOperator(value);
+		} else if (operand2 !== '' && value === '=') {
+			switch (operator) {
+				case '+':
+					setOperand1(Number(operand1) + Number(operand2));
+					setOperator('');
+					setOperand2('');
+					break;
+				case '-':
+					setOperand1(Number(operand1) - Number(operand2));
+					setOperator('');
+					setOperand2('');
+					break;
+				default:
+					break;
+			}
+		} else if (value === 'C') {
+			setOperand1('');
+			setOperator('');
+			setOperand2('');
+		}
+	};
+
 	return (
 		<div className={styles.app}>
-			<div className={styles.display}>123+100</div>
+			<div className={styles.display}>
+				{operand1 !== '' ? `${operand1} ${operator} ${operand2}` : '0'}
+			</div>
 			<ul className={styles.list}>
-				<li className={styles.item + ' ' + styles['item--reset']}>
-					<button className={styles.button + ' ' + styles['button--reset']}>
-						C
-					</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>7</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>8</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>9</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>4</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>5</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>6</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>1</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>2</button>
-				</li>
-				<li className={styles.item}>
-					<button className={styles.button}>3</button>
-				</li>
-				<li className={styles.item + ' ' + styles['item--zero']}>
-					<button className={styles.button}>0</button>
-				</li>
-				<li
-					className={
-						styles.item +
-						' ' +
-						styles['item--operator'] +
-						' ' +
-						styles['item--operator-plus']
-					}
-				>
-					<button className={styles.button + ' ' + styles['button--operator']}>
-						+
-					</button>
-				</li>
-				<li className={styles.item + ' ' + styles['item--operator']}>
-					<button className={styles.button + ' ' + styles['button--operator']}>
-						-
-					</button>
-				</li>
-				<li className={styles.item + ' ' + styles['item--enter']}>
-					<button className={styles.button + ' ' + styles['button--enter']}>
-						=
-					</button>
-				</li>
+				{data.map((item) => (
+					<li
+						className={
+							styles.item +
+							' ' +
+							(item.type === 'zero'
+								? styles['item--zero']
+								: item.type === 'enter'
+									? styles['item--enter']
+									: '')
+						}
+						key={item.value}
+					>
+						<button
+							className={
+								styles.button +
+								' ' +
+								(item.type === 'operator'
+									? styles['button--operator']
+									: item.type === 'reset'
+										? styles['button--reset']
+										: item.type === 'enter'
+											? styles['button--enter']
+											: '')
+							}
+							data-value={item.value}
+							onClick={onClickButton}
+						>
+							{item.value}
+						</button>
+					</li>
+				))}
 			</ul>
 		</div>
 	);
