f79b0ca597c14ba256ad239ebc138b3deabda670
Андрей Хохлов <andrey@MacBook-Air-Andrej.local>
2024-07-04 23:23:16 +0300

    Modified file App.js

--- src/App.js
+++ src/App.js
@@ -6,25 +6,47 @@ export const App = () => {
 	const [operand1, setOperand1] = useState('');
 	const [operator, setOperator] = useState('');
 	const [operand2, setOperand2] = useState('');
+	const [total, setTotal] = useState('0'); // поменять на display или output
+	const [isResult, setResult] = useState(false); // поменять на total
 
 	const onClickButton = ({ target }) => {
 		const value = target.dataset.value;
 		if (Number(value)) {
-			if (operator === '') return setOperand1((operand1) => operand1 + value);
-			setOperand2((operand2) => operand2 + value);
+			if (operator === '') {
+				setOperand1(operand1 + value);
+				setTotal(operand1 + value);
+			} else {
+				setOperand2(operand2 + value);
+				setTotal(`${operand1} ${operator} ${operand2 + value}`);
+			}
 		} else if (value === '+' || value === '-') {
+			// if (!isNaN(Number(total)) && operand2 !== '') {
+			if (isResult) {
+				if (operand1 < 0 && value === '+') {
+					setTotal(`${operand1} - ${Math.abs(operand1)}`);
+					setOperand2(Math.abs(operand1));
+					setOperator('-');
+				} else if (operand1 < 0 && value === '-') {
+					setTotal(`${operand1} + ${Math.abs(operand1)}`);
+					setOperand2(Math.abs(operand1));
+					setOperator('+');
+				} else {
+					setTotal(`${operand1} ${value} ${operand1}`);
+					setOperand2(operand1);
+					setOperator(value);
+				}
+			}
+			setTotal(`${operand1} ${value} ${operand2}`);
 			setOperator(value);
 		} else if (operand2 !== '' && value === '=') {
 			switch (operator) {
 				case '+':
+					setTotal(`${Number(operand1) + Number(operand2)}`);
 					setOperand1(Number(operand1) + Number(operand2));
-					setOperator('');
-					setOperand2('');
 					break;
 				case '-':
+					setTotal(`${Number(operand1) - Number(operand2)}`);
 					setOperand1(Number(operand1) - Number(operand2));
-					setOperator('');
-					setOperand2('');
 					break;
 				default:
 					break;
@@ -33,13 +55,15 @@ export const App = () => {
 			setOperand1('');
 			setOperator('');
 			setOperand2('');
+			setTotal('0');
 		}
 	};
 
 	return (
 		<div className={styles.app}>
 			<div className={styles.display}>
-				{operand1 !== '' ? `${operand1} ${operator} ${operand2}` : '0'}
+				{/* {operand1 !== '' ? `${operand1} ${operator} ${operand2}` : '0'} */}
+				{total}
 			</div>
 			<ul className={styles.list}>
 				{data.map((item) => (
