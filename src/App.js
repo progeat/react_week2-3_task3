c53d90e2df933450db66499fbaea536fe391a2a1
Хохлов Андрей Анатольевич <A.Hohlov@transtelematica.ru>
2024-07-05 16:01:59 +0300

    Modified file App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,13 @@ export const App = () => {
 	const [total, setTotal] = useState('0'); // поменять на display или output
 	const [isResult, setResult] = useState(false); // поменять на total
 
+	console.log(operand1, operator, operand2); // удалить
+
 	const onClickButton = ({ target }) => {
+		// разбить на два обработчика цифр и операторов
 		const value = target.dataset.value;
+
+		// если нажали на цифру
 		if (Number(value)) {
 			if (operator === '') {
 				setOperand1(operand1 + value);
@@ -19,9 +24,11 @@ export const App = () => {
 				setOperand2(operand2 + value);
 				setTotal(`${operand1} ${operator} ${operand2 + value}`);
 			}
+			// если нажали на оператор + или -
 		} else if (value === '+' || value === '-') {
-			// if (!isNaN(Number(total)) && operand2 !== '') {
+			// если было нажато равно
 			if (isResult) {
+				// меняем оператора если первый операнд отрицательное число
 				if (operand1 < 0 && value === '+') {
 					setTotal(`${operand1} - ${Math.abs(operand1)}`);
 					setOperand2(Math.abs(operand1));
@@ -31,31 +38,47 @@ export const App = () => {
 					setOperand2(Math.abs(operand1));
 					setOperator('+');
 				} else {
+					// если нажали равно повторно
 					setTotal(`${operand1} ${value} ${operand1}`);
 					setOperand2(operand1);
 					setOperator(value);
 				}
+			} else {
+				// условие чтобы присвоить первому операнду отрицательное значение
+				if (value === '+' && operand1 === '') return;
+				if (value === '-' && operand1 === '' && operand1 !== '-') {
+					setOperand1('-');
+					setTotal(`-`);
+				} else {
+					// просто добавляем оператора
+					setTotal(`${operand1} ${value} ${operand2}`);
+					setOperator(value);
+				}
 			}
-			setTotal(`${operand1} ${value} ${operand2}`);
-			setOperator(value);
+			// если нажали равно
 		} else if (operand2 !== '' && value === '=') {
+			// выбираем операцию сложения или вычитания в зависимости от оператора
 			switch (operator) {
 				case '+':
 					setTotal(`${Number(operand1) + Number(operand2)}`);
-					setOperand1(Number(operand1) + Number(operand2));
+					setOperand1(`${Number(operand1) + Number(operand2)}`);
+					setResult(true);
 					break;
 				case '-':
 					setTotal(`${Number(operand1) - Number(operand2)}`);
-					setOperand1(Number(operand1) - Number(operand2));
+					setOperand1(`${Number(operand1) - Number(operand2)}`);
+					setResult(true);
 					break;
 				default:
 					break;
 			}
+			// делаем сброс если нажали кнопку сброса
 		} else if (value === 'C') {
 			setOperand1('');
 			setOperator('');
 			setOperand2('');
 			setTotal('0');
+			setResult(false);
 		}
 	};
 
